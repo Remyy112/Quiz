132b1dbb808d42f6e4071627519c6cce29aa095d
Remyy112 <remigiuszpawlowski05@gmail.com>
2022-10-12 13:47:01 +0200

    zapis1

--- quiz.php
+++ quiz.php
@@ -5,13 +5,27 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-    <?php
-        $con = new mysqli("127.0.0.1", "root", "", "quiz");
-        $res = $con->query("SELECT id FROM questions");
-    ?>
-    
     <div class="header">
         Quiz
+    </div>
+    <div class="body1">
+        <?php
+            $con = new mysqli("127.0.0.1", "root", "", "quiz");
+            $res = $con->query("SELECT question FROM questions WHERE id=6");
+            $rows = $res->fetch_all(MYSQLI_ASSOC);
+                for($i=0; $i<count($rows); $i++){
+                    echo $rows[$i]["question"]."<br>";
+                }
+        ?>
+    </div>
+    <div class="body2">
+        <?php
+            $con = new mysqli("127.0.0.1", "root", "", "quiz");
+            $res = $con->query("SELECT answers FROM `answers` WHERE id=11 OR id=12 OR id=13 OR id=14;");
+            $rows = $res->fetch_all(MYSQLI_ASSOC);
+                for($i=0; $i<count($rows); $i++){
+                    echo "<button>".$rows[$i]["answers"]."</button>";
+                }
+        ?>
     </div>
 </body>
